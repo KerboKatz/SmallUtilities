60351cb41e0a3d86597ba4685d26d2b27a05feaf
Xarun <Xarun@users.noreply.github.com>
2015-05-02 22:22:29 +0200

    1.1.3
    * Added:
    * DisableTempGagues: Disables the temperature gauges on start

---
+++ source/DisableTempGagues/DisableTempGagues.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace KerboKatz
+{
+  [KSPAddon(KSPAddon.Startup.Flight, false)]
+  public partial class DisableTempGagues : MonoBehaviour
+  {
+    private bool setToFalse;
+    public void Update()
+    {
+      if (!setToFalse)
+      {
+        if (TemperatureGagueSystem.Instance != null)
+        {
+          TemperatureGagueSystem.Instance.showGagues = false;
+          setToFalse = true;
+        }
+      }
+    }
+  }
+}
