e4c690b17878a45a8d7a47ab03ccf26b160df211
Xarun <Xarun@users.noreply.github.com>
2015-06-04 07:27:59 +0200

    v 1.1.12
    * Added PhysicalTimeRatioViewer
    * Removed final from ModuleManager cfg file for Afterburner module

---
+++ source/PhysicalTimeRatioViewer/PhysicalTimeRatioViewer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using UnityEngine;
+
+namespace KerboKatz
+{
+  [KSPAddon(KSPAddon.Startup.Flight, false)]
+  public partial class PhysicalTimeRatioViewer : KerboKatzBase
+  {
+    private float thisTime;
+    private float lastTime;
+    private float ThisRealTime;
+    private float lastRealTime;
+    private float lastGametimeToRealtimeCheck;
+    private float gameTimeToRealtime;
+    public PhysicalTimeRatioViewer()
+    {
+      modName = "PhysicalTimeRatioViewer";
+      displayName = "Physical time ratio viewer";
+      tooltip = "Use left click to show/hide the physical time ratio viewer overlay.\n Use right click to open the settings menu.";
+      requiresUtilities = new Version(1, 2, 0);
+    }
+
+    protected override void Started()
+    {
+      currentSettings.load("SmallUtilities/PhysicalTimeRatioViewer", "PhysicalTimeRatioViewerSettings", modName);
+      position.x = currentSettings.getFloat("gaugePosX");
+      position.y = currentSettings.getFloat("gaugePosY");
+
+      setIcon(Utilities.getTexture("icon", "SmallUtilities/PhysicalTimeRatioViewer/Textures"));
+      setAppLauncherScenes(ApplicationLauncher.AppScenes.ALWAYS);
+      GameEvents.onGamePause.Add(onPause);
+      GameEvents.onGameUnpause.Add(onUnpause);
+    }
+
+    private void onUnpause()
+    {
+      lastGametimeToRealtimeCheck = Time.realtimeSinceStartup;
+      thisTime = Time.time;
+      ThisRealTime = Time.realtimeSinceStartup;
+    }
+
+    private void onPause()
+    {
+      gameTimeToRealtime = 0;
+    }
+
+    public void FixedUpdate()
+    {
+      if (lastGametimeToRealtimeCheck + 0.25f < Time.realtimeSinceStartup)
+      {
+        lastGametimeToRealtimeCheck = Time.realtimeSinceStartup;
+        lastTime = thisTime;
+        thisTime = Time.time;
+        lastRealTime = ThisRealTime;
+        ThisRealTime = Time.realtimeSinceStartup;
+        gameTimeToRealtime = Mathf.Round((thisTime - lastTime) / (ThisRealTime - lastRealTime) * 100);
+      }
+    }
+
+    protected override void onToolbar()
+    {
+      if (Input.GetMouseButtonUp(0))
+      {
+        if (currentSettings.getBool("showGauge"))
+        {
+          currentSettings.set("showGauge", false);
+        }
+        else
+        {
+          currentSettings.set("showGauge", true);
+        }
+      }
+      else if (Input.GetMouseButtonUp(1))
+      {
+        toggleSettingsWindow();
+      }
+    }
+
+    private void toggleSettingsWindow()
+    {
+      if (currentSettings.getBool("showSettings"))
+      {
+        currentSettings.set("showSettings", false);
+      }
+      else
+      {
+        currentSettings.set("showSettings", true);
+      }
+    }
+
+    protected override void beforeSaveOnDestroy()
+    {
+      GameEvents.onGamePause.Remove(onPause);
+      GameEvents.onGameUnpause.Remove(onUnpause);
+      if (currentSettings != null)
+      {
+        currentSettings.set("showSettings", false);
+        currentSettings.set("changePosition", false);
+        currentSettings.set("gaugePosX", position.x);
+        currentSettings.set("gaugePosY", position.y);
+      }
+    }
+  }
+}
---
+++ source/PhysicalTimeRatioViewer/PhysicalTimeRatioViewerUI.cs
@@ -0,0 +1,140 @@
+﻿using KerboKatz.Extensions;
+using UnityEngine;
+
+namespace KerboKatz
+{
+  public partial class PhysicalTimeRatioViewer : KerboKatzBase
+  {
+    private bool hideOnUIHidden;
+    private bool initStyle;
+    private GUIStyle buttonStyle;
+    private GUIStyle changePositionStyle;
+    private GUIStyle gaugeStyle;
+    private GUIStyle moveHere;
+    private GUIStyle settingsWindowStyle;
+    private GUIStyle toggleStyle;
+    private Rectangle position = new Rectangle();
+    private Rectangle settingsWindowRect = new Rectangle(Rectangle.updateType.Cursor);
+    private static int changePositionWindowID = Utilities.UI.getNewWindowID;
+    private static int settingsWindowID = Utilities.UI.getNewWindowID;
+
+    private void InitStyle()
+    {
+      settingsWindowStyle = new GUIStyle(HighLogic.Skin.window);
+      settingsWindowStyle.fixedWidth = 250;
+
+      changePositionStyle = new GUIStyle(HighLogic.Skin.window);
+      changePositionStyle.fixedHeight = 50;
+      changePositionStyle.fixedWidth = 50;
+      changePositionStyle.border.top = 0;
+      changePositionStyle.padding.setToZero();
+      changePositionStyle.contentOffset = new Vector2(0, 0);
+
+      buttonStyle = new GUIStyle(HighLogic.Skin.button);
+      buttonStyle.fixedWidth = 115;
+
+      toggleStyle = new GUIStyle(HighLogic.Skin.toggle);
+
+      gaugeStyle = new GUIStyle(HighLogic.Skin.label);
+      gaugeStyle.margin.setToZero();
+      gaugeStyle.padding.setToZero();
+      gaugeStyle.fixedHeight = 50;
+      gaugeStyle.fixedWidth = 50;
+      gaugeStyle.alignment = TextAnchor.MiddleCenter;
+
+      moveHere = new GUIStyle(gaugeStyle);
+      moveHere.alignment = TextAnchor.UpperLeft;
+
+      initStyle = true;
+    }
+
+    public void OnGUI()
+    {
+      if (!initStyle)
+        InitStyle();
+      if (currentSettings.getBool("showGauge"))
+      {
+        showGaugeOnDisplay();
+      }
+      Utilities.UI.createWindow(currentSettings.getBool("showSettings"), settingsWindowID, ref settingsWindowRect, settingsWindow, "Physical time ratio viewer", settingsWindowStyle);
+      Utilities.UI.createWindow(currentSettings.getBool("changePosition"), changePositionWindowID, ref position, changePosition, "", changePositionStyle);
+      Utilities.UI.showTooltip();
+    }
+
+    private void showGaugeOnDisplay()
+    {
+      if (UIEvents._instance.hiddenUI && currentSettings.getBool("hideOnUIHidden"))
+      {
+        return;
+      }
+      GUI.Label(position.rect, gameTimeToRealtime + "%", gaugeStyle);
+      if (currentSettings.getBool("changePosition"))
+      {
+        GUI.depth = int.MaxValue;
+      }
+      else
+      {
+        GUI.depth = currentSettings.getInt("depth");
+      }
+    }
+
+    private void settingsWindow(int id)
+    {
+      if (Utilities.UI.createToggle("Always on top", (currentSettings.getInt("depth") == 0), toggleStyle))
+      {
+        currentSettings.set("depth", 0);
+      }
+      else
+      {
+        currentSettings.set("depth", int.MaxValue - 1);
+      }
+      if (Utilities.UI.createToggle("Hide on hidden UI", hideOnUIHidden, toggleStyle))
+      {
+        hideOnUIHidden = true;
+      }
+      else
+      {
+        hideOnUIHidden = false;
+      }
+      GUILayout.BeginVertical();
+      Utilities.UI.createOptionSwitcher("Use:", Toolbar.toolbarOptions, ref toolbarSelected);
+
+      GUILayout.BeginHorizontal();
+      GUILayout.FlexibleSpace();
+      if (Utilities.UI.createButton("Change position", buttonStyle, "Press on this to change the position where the gauge will show."))
+      {
+        if (currentSettings.getBool("changePosition"))
+        {
+          currentSettings.set("changePosition", false);
+        }
+        else
+          currentSettings.set("changePosition", true);
+      }
+      GUILayout.FlexibleSpace();
+      GUILayout.EndHorizontal();
+
+      GUILayout.BeginHorizontal();
+      if (Utilities.UI.createButton("Save", buttonStyle))
+      {
+        currentSettings.set("gaugePosX", position.x);
+        currentSettings.set("gaugePosY", position.y);
+        currentSettings.set("hideOnUIHidden", hideOnUIHidden);
+        updateToolbarBool();
+      }
+      GUILayout.FlexibleSpace();
+      if (Utilities.UI.createButton("Close", buttonStyle))
+      {
+        toggleSettingsWindow();
+      }
+      GUILayout.EndHorizontal();
+      GUILayout.EndVertical();
+      Utilities.UI.updateTooltipAndDrag();
+    }
+
+    private void changePosition(int id)
+    {
+      GUILayout.Label(new GUIContent(Utilities.getTexture("moveHere", "SmallUtilities/Textures"), "Drag this window to where you want the gauge to be"), moveHere);
+      Utilities.UI.updateTooltipAndDrag();
+    }
+  }
+}
